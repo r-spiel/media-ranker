ac60617d7b2627cb9799d6ee296e4e135e60f40a
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-15 14:10:36 -0800

    finish vote model tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do
+    before do
+      @vote = votes(:vote0)
+    end
+
+    it 'is valid with a user and work' do
+      expect(@vote.valid?).must_equal true
+    end
+
+    it 'is invalid without a user' do
+      @vote.user = nil
+
+      expect(@vote.valid?).must_equal false
+    end
+
+    it 'must have a work' do
+      @vote.work = nil
+
+      expect(@vote.valid?).must_equal false
+    end
+  end
+
+  describe 'validations' do
+    it 'a user cannot vote for the same work twice' do
+      user = users(:user0)
+      work = works(:album0)
+      new_vote = Vote.new(user: user, work: work)
+
+      expect(new_vote.valid?).must_equal false
+    end
+  end
 end
