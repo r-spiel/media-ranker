14d546b295e36fc8198419c37d676e53e793bb89
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-15 22:57:42 -0800

    update link in works index and route order

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'works#homepage'
+
   resources :works do
     resources :votes, only: [:create]
   end
-
-  #custom route for homepage:
-  root :to => 'works#homepage'
 
   get '/login', to: 'users#login_form', as: 'login'
   post '/login', to: 'users#login'
